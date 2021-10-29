c2b4c41a6c1e27cf05b2a11fa602efcd2bbbb658
carla regina <carlareginam10@gmail.com>
2021-10-29 20:11:36 -0300

    atualização atividade

--- src/test/java/fit/challenge/ImcCalculator.java
+++ src/test/java/fit/challenge/ImcCalculator.java
@@ -4,8 +4,8 @@ public class ImcCalculator {
     public static String calculate(double weitgh, double height){
         double result = 0.0;
         result = weitgh /Math.pow(height , 2);
-        if (result  < 18.5 ) return "magreza";
-        if (result >= 18.5 && result < 24.9 ) return "normal";
+        if (result  < 18.5 ) return "Magreza";
+        if (result >= 18.5 && result < 24.9 ) return "Normal";
         if (result >= 24.9 && result < 30 ) return "Sobrepeso";
         return "Obesidade";
 
